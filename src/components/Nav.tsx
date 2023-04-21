e4001eafed2aad4ffb20e4cb855bdf9fbae66fc3
Habyte <qwaszxed1234@gmail.com>
2023-04-22 00:06:23 +0900

    Feat: 프로젝트 기본 구조 설정
    react router dom으로 기본 페이지 및 라우트 구조 설계
    기본 CSS 설정 및 상수 변수 설정
    메인 페이지 기본 골조 설계
    
    Ref: #2

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,99 @@
 import React from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+
+import styled from 'styled-components';
+import { BREAK_POINT, FONT_WEIGHT } from '../constants/style';
+import logoImg from '../assets/logo.png';
+import mapImg from '../assets/map-logo.png';
+import homiImg from '../assets/homi-logo.png';
+
+const Container = styled.div`
+  margin: 0 auto;
+  padding: 0 1rem;
+  max-width: 1200px;
+
+  @media (min-width: ${BREAK_POINT.MOBILE}) {
+    padding: 0 1.5rem;
+  }
+
+  @media (min-width: ${BREAK_POINT.LABTOP}) {
+    padding: 0 2rem;
+  }
+`;
+
+const Navbar = styled.div`
+  margin-top: 70px;
+  position: relative;
+  height: 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const LogoImageContainer = styled.div`
+  display: flex;
+  flex-shrink: 0;
+  align-items: center;
+  cursor: pointer;
+`;
+
+const LogoImage = styled.img`
+  display: block;
+  width: auto;
+  height: 40px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-shrink: 0;
+  align-items: center;
+`;
+
+const MapButton = styled.button`
+  margin-left: 30px;
+  display: flex;
+  align-items: center;
+
+  &:hover {
+    transform: translateY(-3px);
+    transition: all 0.2s ease-in;
+  }
+`;
+
+const ButtonImage = styled.img`
+  height: 28px;
+`;
+
+const ButtonSpan = styled.span`
+  margin-left: 10px;
+  font-weight: ${FONT_WEIGHT.BOLD};
+`;
 
 const Nav = () => {
-  return <h1>Nav</h1>;
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Container>
+        <Navbar>
+          <LogoImageContainer onClick={() => navigate(`/`)}>
+            <LogoImage src={logoImg} alt="로고" />
+          </LogoImageContainer>
+          <ButtonContainer>
+            <MapButton onClick={() => navigate(`/map`)}>
+              <ButtonImage src={mapImg} alt="맵아이콘" />
+              <ButtonSpan>내 주변 분양</ButtonSpan>
+            </MapButton>
+            <MapButton onClick={() => navigate(`/my`)}>
+              <ButtonImage src={homiImg} alt="맵아이콘" />
+              <ButtonSpan>마이페이지</ButtonSpan>
+            </MapButton>
+          </ButtonContainer>
+        </Navbar>
+      </Container>
+      <Outlet />
+    </>
+  );
 };
 
 export default Nav;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,31 @@
+import './reset.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Nav from './components/Nav';
+import Main from './pages/Main/Main';
+import Map from './pages/Map/Map';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Nav />,
+    children: [
+      {
+        index: true,
+        element: <Main />,
+      },
+      {
+        path: '/map',
+        element: <Map />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
