f52d79428735081f00d533a6c163fc77ddc75a82
yira <dladlfk1@gmail.com>
2023-06-14 22:53:07 +0900

    Feat: 수정(#8)

--- src/pages/My/Post.tsx
+++ src/pages/My/Post.tsx
@@ -12,13 +12,13 @@ interface Idata {
 
 function Post({ data }: Idata) {
   const nav = useNavigate();
-  console.log(data)
+  console.log(data);
   return (
     <PostContainer onClick={() => nav(`/my/${data.id}`)}>
       <ImageContainer>
         {data.images.length === 0 ? (
           <EmptyImg>
-             <Image src={noImgIcon} alt="이미지 없음" />
+            <Image src={noImgIcon} alt="이미지 없음" />
           </EmptyImg>
         ) : (
           <Image src={data.images[0]} alt="텃밭 이미지" />
@@ -30,8 +30,8 @@ function Post({ data }: Idata) {
           {data.status === 'ACTIVE' && <Dot />}
           {data.status === 'ACTIVE' && <Text>모집 중</Text>}
           {data.status === 'INACTIVE' && <Text>마감</Text>}
-          {data.status === 'ALWAYS_ACTIVE' && <Text>상시</Text>}
-          {data.status === null && <Text>정보없음</Text>}
+          {data.status === 'ALWAYS_ACTIVE' && <Text>상시 모집</Text>}
+          {data.status === null && <Text>상시 모집</Text>}
         </Status>
         <Title>{data.name}</Title>
         <Value style={{ color: '#afafaf' }}>{data.size} 평</Value>
@@ -71,7 +71,7 @@ const ImageContainer = styled.div`
 `;
 
 const EmptyImg = styled.div`
-  background: #F0FBE4;
+  background: #f0fbe4;
   border-radius: 8px;
   width: 174px;
   height: 135px;
@@ -82,7 +82,6 @@ const EmptyImg = styled.div`
     height: 45px;
     width: 45px;
   }
- 
 `;
 
 const Image = styled.img`
