df53ac34be6a98c70ddf40452ecada083494b9bd
mac <mac@macui-MacBookAir.local>
2023-06-20 15:36:26 +0900

    url 수정

--- src/pages/Login/Google/token/token.tsx
+++ src/pages/Login/Google/token/token.tsx
@@ -12,7 +12,6 @@ export const getToken = async (token: string) => {
     redirect_uri: process.env.REACT_APP_GOOGLE_REDIRECT_URL,
     grant_type: 'authorization_code',
   });
-  console.log(process.env.REACT_APP_GOOGLE_CLIENT_SECRET);
   const data: IData = res_google.data;
   const res_server: AxiosResponse = await axios.get<IData_Sever>(`${process.env.REACT_APP_API_BASE_URL}auth/google`, {
     headers: {
