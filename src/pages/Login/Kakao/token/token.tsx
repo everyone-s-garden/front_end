80bdf1ee0ea59cefa5995fff8b230a4910ca3ae7
limyira <dladlfk1@gmail.com>
2023-12-17 11:25:18 +0900

    카카오 로그인 client_secret 추가

--- src/pages/Login/Kakao/token/token.tsx
+++ src/pages/Login/Kakao/token/token.tsx
@@ -11,35 +11,41 @@ const Token = () => {
   const nav = useNavigate();
   const setIsLogin = useSetRecoilState<boolean>(isLoginAtom);
   const getCode = async () => {
-    const code: string | null = new URLSearchParams(window.location.search).get('code');
-
-    const res_kakao: AxiosResponse = await axios.post<IData>(
-      `https://kauth.kakao.com/oauth/token`,
-      {
-        grant_type: 'authorization_code',
-        client_id: process.env.REACT_APP_KAKAO_REST_API_KEY,
-        code: code,
-        client_secret: process.env.REACT_APP_API_KAKAO_CLIENT_SECRET,
-        redirect_uri: process.env.REACT_APP_KAKAO_REDIRECT_URI,
-      },
-      {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
+    try {
+      const code: string | null = new URLSearchParams(window.location.search).get('code');
+      const res_kakao: AxiosResponse = await axios.post<IData>(
+        `https://kauth.kakao.com/oauth/token`,
+        {
+          grant_type: 'authorization_code',
+          client_id: process.env.REACT_APP_KAKAO_REST_API_KEY,
+          code: code,
+          client_secret: process.env.REACT_APP_API_KAKAO_CLIENT_SECRET,
+          redirect_uri: process.env.REACT_APP_KAKAO_REDIRECT_URI,
         },
-      },
-    );
-    const data: IData = res_kakao.data;
-    const res_server: AxiosResponse = await axios.get<IData_Sever>(`${process.env.REACT_APP_API_BASE_URL}auth/kakao`, {
-      headers: {
-        Authorization: `${data.access_token}`,
-      },
-    });
-    setItem('name', res_server.data.name);
-    setItem('userId', res_server.data.userId);
-    setItem('access_token', res_server.data.appToken);
-    setItem('isLogin', 'true');
-    setIsLogin(true);
-    nav('/');
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        },
+      );
+      const data: IData = res_kakao.data;
+      const res_server: AxiosResponse = await axios.get<IData_Sever>(
+        `${process.env.REACT_APP_API_BASE_URL}auth/kakao`,
+        {
+          headers: {
+            Authorization: `${data.access_token}`,
+          },
+        },
+      );
+      setItem('name', res_server.data.name);
+      setItem('userId', res_server.data.userId);
+      setItem('access_token', res_server.data.appToken);
+      setItem('isLogin', 'true');
+      setIsLogin(true);
+      nav('/');
+    } catch (err) {
+      console.log(err);
+    }
   };
   useEffect(() => {
     getCode();
