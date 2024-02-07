a9254425fb65afb05b124be8abfc87b20eb6c367
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2024-02-08 02:26:23 +0900

    style: 레이아웃 스타일 적용

--- src/pages/CommunityWrite/CommunityWrite.tsx
+++ src/pages/CommunityWrite/CommunityWrite.tsx
@@ -5,6 +5,8 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 import CameraIcon from '../../assets/community/camera.svg';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { BREAK_POINT } from 'constants/style';
+import theme from 'styles/theme';
 
 type Inputs = {
   title: string;
@@ -66,16 +68,18 @@ const CommunityWrite = () => {
       <Helmet>
         <title>속닥속닥 글쓰기 페이지</title>
       </Helmet>
-      <div>{/* 툴바 */}</div>
+      <ToolBar>{/* 툴바 */}툴바</ToolBar>
 
-      <input placeholder="제목" {...register('title', { required: '필수 입력 항목입니다.' })} />
-      {errors.title && <p>{errors.title.message}</p>}
+      <Content>
+        <input placeholder="제목" {...register('title', { required: '필수 입력 항목입니다.' })} />
+        <div>{errors.title && <p>{errors.title.message}</p>}</div>
 
-      <textarea
-        placeholder="질문, 자랑, 공유 등 다양한 글을 작성해보세요."
-        {...register('content', { required: '필수 입력 항목입니다.' })}
-      />
-      {errors.content && <p>{errors.content.message}</p>}
+        <textarea
+          placeholder="질문, 자랑, 공유 등 다양한 글을 작성해보세요."
+          {...register('content', { required: '필수 입력 항목입니다.' })}
+        />
+        <div>{errors.content && <p>{errors.content.message}</p>}</div>
+      </Content>
 
       <ImageContainer>
         <ImageBox>
@@ -83,12 +87,14 @@ const CommunityWrite = () => {
             type="file"
             id="fileInput"
             multiple={true}
-            accept="image/jpg,impge/png,image/jpeg,image/gif"
+            accept="image/jpg,image/png,image/jpeg,image/gif"
             {...register('images')}
           />
           <ImageAdder htmlFor="fileInput">
-            <img src={CameraIcon} alt="사진 추가 아이콘" />
-            <p>{imageFiles.length} / 10</p>
+            <div>
+              <img src={CameraIcon} alt="사진 추가 아이콘" />
+              <p>{imageFiles.length}/10</p>
+            </div>
           </ImageAdder>
         </ImageBox>
 
@@ -100,7 +106,7 @@ const CommunityWrite = () => {
         ))}
       </ImageContainer>
 
-      <input type="submit" />
+      <SubmitBtn type="submit" value="등록하기" />
     </Container>
   );
 };
@@ -110,13 +116,67 @@ export default CommunityWrite;
 const Container = styled.form`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  gap: 35px;
   margin: 0 auto;
   max-width: 1440px;
   width: 100%;
+  height: 80vh;
+  margin-bottom: 100px;
+`;
+
+const ToolBar = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  justify-content: center;
+  border-top: 1px solid #000;
+  border-bottom: 1px solid #000;
+  height: 64px;
+  flex-shrink: 0;
+`;
+
+const Content = styled.div`
+  border-bottom: 1px solid #000;
+  flex-shrink: 1;
+  width: 100%;
+  max-width: 1188px;
+
+  & input {
+    height: 48px;
+    width: 100%;
+    font-size: 24px;
+    font-weight: 600;
+    border: none;
+    border-bottom: 1px solid #000;
+  }
+
+  & textarea {
+    width: 100%;
+    height: 550px;
+    resize: none;
+    border: none;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  & div {
+    height: 20px;
+    margin-bottom: 15px;
+  }
+
+  & p {
+    font-size: 14px;
+  }
 `;
 
 const ImageContainer = styled.ul`
   display: flex;
+  gap: 14px;
+  width: 100%;
+  max-width: 1188px;
 `;
 
 const ImageInput = styled.input`
@@ -126,7 +186,9 @@ const ImageInput = styled.input`
 const ImageBox = styled.li`
   width: 100px;
   height: 100px;
-  border: 1px solid #000;
+  position: relative;
+  border-radius: 10px;
+  background-color: ${theme.colors.orange[100]};
 `;
 
 const ImageAdder = styled.label`
@@ -134,12 +196,59 @@ const ImageAdder = styled.label`
   width: 100%;
   height: 100%;
   cursor: pointer;
+  border-radius: 10px;
+
+  &:hover {
+    border: 1px solid ${theme.colors.red};
+  }
+
+  & div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    gap: 8px;
+  }
+
+  & p {
+    font-size: 16px;
+    color: ${theme.colors.orange[600]};
+  }
 `;
 
-const ImageDelBtn = styled.button``;
+const ImageDelBtn = styled.button`
+  position: absolute;
+  top: -5px;
+  right: -5px;
+  background-color: white;
+  border-radius: 50%;
+  padding: 5px;
+  width: 20px;
+  height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 100;
+`;
 
 const Image = styled.img`
   width: 100%;
   height: 100%;
+  border-radius: 10px;
   object-fit: cover;
+`;
+
+const SubmitBtn = styled.input`
+  width: 350px;
+  height: 56px;
+  flex-shrink: 0;
+  border-radius: 10px;
+  margin: 0 auto;
+  background-color: ${theme.colors.orange[600]};
+  color: #fff;
+  font-size: 18px;
+  font-weight: 600;
+  border: none;
+  cursor: pointer;
 `;
