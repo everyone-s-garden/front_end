c4fb614ac54db5ab7ca1cc6ca63b385534728de4
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2024-02-11 13:42:51 +0900

    style: 커뮤니티 아이콘 추가

---
+++ src/assets/community/index.ts
@@ -0,0 +1,8 @@
+export { ReactComponent as ArrowBelowIcon } from './arrow-below.svg';
+export { ReactComponent as AlignCenterIcon } from './align-center.svg';
+export { ReactComponent as AlignLeftIcon } from './align-left.svg';
+export { ReactComponent as AlignRightIcon } from './align-right.svg';
+export { ReactComponent as BoldIcon } from './bold.svg';
+export { ReactComponent as ItalicIcon } from './italic.svg';
+export { ReactComponent as UnderlineIcon } from './underline.svg';
+export { ReactComponent as CameraIcon } from './camera.svg';
