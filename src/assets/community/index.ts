c93b1a57cddf3a412b40a0354db7c74fcb5be541
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2024-02-13 06:37:57 +0900

    style: 아이콘 추가

--- src/assets/community/index.ts
+++ src/assets/community/index.ts
@@ -1,4 +1,6 @@
 export { ReactComponent as ArrowBelowIcon } from './arrow-below.svg';
+export { ReactComponent as ArrowLeftIcon } from './arrow-left.svg';
+export { ReactComponent as ArrowRightIcon } from './arrow-right.svg';
 export { ReactComponent as AlignCenterIcon } from './align-center.svg';
 export { ReactComponent as AlignLeftIcon } from './align-left.svg';
 export { ReactComponent as AlignRightIcon } from './align-right.svg';
@@ -6,3 +8,4 @@ export { ReactComponent as BoldIcon } from './bold.svg';
 export { ReactComponent as ItalicIcon } from './italic.svg';
 export { ReactComponent as UnderlineIcon } from './underline.svg';
 export { ReactComponent as CameraIcon } from './camera.svg';
+export { ReactComponent as DeleteIcon } from './delete.svg';
